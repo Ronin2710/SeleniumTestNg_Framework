20308d8198936763637cbb73600fa706dcee2690
Ronin2710 <piyushchourse05@gmail.com>
2023-07-05 06:58:49 +0530

    my first commit

---
+++ src/main/java/Demo/thmarch2023testng/App.java
@@ -0,0 +1,13 @@
+package Demo.thmarch2023testng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
